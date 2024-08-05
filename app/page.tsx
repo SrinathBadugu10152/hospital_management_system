07677ca378f7fe5ece74483cd5ebe9ffb342e37a
srinath badugu <srinathbadugu10152@gmail.com>
2024-08-05 17:55:39 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Home() {
+  return (
+    <section className="text-3xl flex min-h-screen justify-center items-center">
+      <h1 className="font-semibold">Medical app</h1>
+    </section>
+  );
+}
